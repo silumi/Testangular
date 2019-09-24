57870c0f61678735e7125f6d1f81262a84acb62d
silumi <silumikaundika@gmail.com>
2019-09-24 09:03:33 +0530

    Data binding- Event binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    CockpitComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
